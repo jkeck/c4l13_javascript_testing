56da8ff70b1a5655c46d6c5c48ce4bfabea2fda7
Jessie Keck <jessie.keck@gmail.com>
2013-02-09 18:34:03 -0800

    Initial commit of rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+C4lWatir::Application.config.secret_token = '6f23fdf95df9c126b2abb59fe925b5dbac3e606a5427f52bd9cdfb3d64d6950a13f9bbbaa7a6db0be3cf8e316921e344284743764dadb7a33206d25497097c80'
