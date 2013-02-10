6736a1618bb34d6b1b5c1951521572fdace9db64
Jessie Keck <jessie.keck@gmail.com>
2013-02-09 20:54:34 -0800

    Add watir feature for facets.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'rspec'
 gem 'rspec-rails'
 gem 'capybara'
 
+gem 'watir-rails'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
---
+++ spec/features/javascript/facets_spec.rb
@@ -0,0 +1,19 @@
+require "watir_helper"
+
+describe "Facets", :javascript => true do
+  before :all do 
+    @browser = Watir::Browser.new
+  end
+  after :all do
+    @browser.close if @browser
+  end
+  it "should exist on the home page and lead to a search result" do
+    @browser.goto(root_path)
+    @browser.h5(:text, "Topic").should exist
+    @browser.h5(:text, "Topic").click
+    
+    @browser.link(:text, "Japanese drama").should exist
+    @browser.link(:text, "Japanese drama").click
+    @browser.text.should include "1 - 2 of 2"
+  end
+end
